e3848a45fc46b166303768a25e8a858de6e1186b
Patrickrrr007 <33652098+Patrickrrr007@users.noreply.github.com>
2025-04-13 01:14:29 +0000

    PE01

---
+++ PE01/app/(tabs)/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>ChaoKai Lin</Text>
+      <Text style={styles.text}>MSCS</Text>
+      <Text style={styles.text}>STC</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: 'black',
+    marginVertical: 4,
+  },
+});
